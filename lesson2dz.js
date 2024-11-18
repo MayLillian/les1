7d3c86b0b6cecc20cf31578842b78b4716efe341
Liliya <May.lillian1110@gmail.com>
2024-11-18 13:36:03 +0300

    +

---
+++ lesson2dz.js
@@ -0,0 +1,24 @@
+let name = "Liliya";
+let age = 32;
+let isStudent = true;
+let height = 164;
+let hobby = "knitting";
+let favoriteNumber = 11;
+let isGraduated = false;
+
+const nameType = typeof name;
+const ageType = typeof age;
+const isStudentType = typeof isStudent;
+const heightType = typeof height;
+const hobbyType = typeof hobby;
+const favoriteNumberType = typeof favoriteNumber;
+const isGraduatedType = typeof isGraduated;
+
+console.log(`${nameType}:${name}, ${ageType}:${age}, ${isStudentType}:${isStudent}, ${heightType}:${height}, ${hobbyType}:${hobby}, ${favoriteNumberType}:${favoriteNumber}, ${isGraduatedType}:${isGraduated}`);
+
+let myVariable;
+console.log(myVariable);
+let implicitUndefined;
+let NullVariable = null;
+console.log(NullVariable);
+
