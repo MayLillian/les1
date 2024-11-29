5931105e1d2d8d015173e6a0daaab416728756b2
Liliya <May.lillian1110@gmail.com>
2024-11-29 16:40:08 +0300

    +

--- lesson11.js
+++ lesson11.js
@@ -1,2 +1,10 @@
 console.log("Hello World");
 
+function diff(number) {
+    let firstDigit = number / 10;
+    let secondDigit = number % 10;
+    let difference = Math.abs(firstDigit - secondDigit);
+    return difference;
+}
+let result = diff(23);
+console.log(result);
